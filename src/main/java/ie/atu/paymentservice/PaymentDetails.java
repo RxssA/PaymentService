185dad8a5dd401878dd56e5cefa6e543da08b742
rossa <g00401655@atu.ie>
2024-11-10 14:03:05 +0000

    added PaymentDetails

--- src/main/java/ie/atu/paymentservice/PaymentDetails.java
+++ src/main/java/ie/atu/paymentservice/PaymentDetails.java
@@ -1,4 +1,39 @@
 package ie.atu.paymentservice;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class PaymentDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private Long bookingId;
+    private String paymentStatus;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
 }
