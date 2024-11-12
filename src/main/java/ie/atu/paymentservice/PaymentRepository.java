0195d43daf6fdd0a68e226569ee945ba01cf34e9
rossa <g00401655@atu.ie>
2024-11-12 20:14:13 +0000

    updated to mongodb

--- src/main/java/ie/atu/paymentservice/PaymentRepository.java
+++ src/main/java/ie/atu/paymentservice/PaymentRepository.java
@@ -1,8 +1,9 @@
 package ie.atu.paymentservice;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<PaymentDetails, Long> {
+public interface PaymentRepository extends MongoRepository<PaymentDetails, String> {
+
 }
